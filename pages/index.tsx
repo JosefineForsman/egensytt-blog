5b317d701e2fea6c24ad6b6f75a0f3b3c33a2ee0
Josefine Forsman <josefineforsman1@gmail.com>
2024-02-21 12:04:11 +0100

    contentful is implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
 
-export default function BlogPosts(){
+import { createClient } from "contentful"
+
+export async function getStaticProps(){
+    const client = createClient({
+        space: process.env.CONTENTFUL_SPACE_ID as string,
+        accessToken: process.env.CONTENTFUL_ACCESS_KEY as string,
+    })
+
+    const res = await client.getEntries({ content_type: 'blogPost' })
+
+    return {
+        props: {
+            blogPosts: res.items
+        }
+    }
+}
+export default function BlogPosts({ blogPosts }: any){
+    console.log(blogPosts)
     return(
         <div className="blog-posts-list">
             Blogpost list
