997b63e555b53481178ab782d8ab733327ff5624
Josefine Forsman <josefineforsman1@gmail.com>
2024-06-07 19:10:08 +0200

    implement contact from in contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,26 @@
-function Contact() {
-    return (
-      <div>
-        <h1>Kontakt</h1>
-        <p>Detta är kontakt sidan.</p>
-      </div>
-    )
-  }
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Box } from '@mui/material';
 
-  export default Contact;
+function Contact() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Box sx={{width:"650px", display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center" }}>
+      <h1>Kontakt</h1>
+      <p>Vill du komma i kontakt med oss? Skicka gärna ett mail eller kontakta oss på våra sociala medier.</p>
+      <form onSubmit={handleSubmit}>
+        <TextField label="Namn" variant="outlined" fullWidth margin="normal" />
+        <TextField label="Email" variant="outlined" fullWidth margin="normal" />
+        <TextField label="Meddelande" variant="outlined" multiline rows={4} fullWidth margin="normal" />
+        <Button type="submit" variant="contained" color="primary">
+          Skicka
+        </Button>
+      </form>
+    </Box>
+  );
+}
+
+export default Contact;
